176ee87f5dfd51af0a2aafcc63f92e83a831aa9f
Saturnus1323 <116801949+Saturnus1323@users.noreply.github.com>
2023-09-18 16:09:44 +0700

    Add files via upload

---
+++ Tugas2.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas2;
+
+/**
+ *
+ * @author Administrasi
+ */
+public class Tugas2 {
+
+    public static void main(String args[]){ 
+            String Nama = "Syaira Amani Fatihah";
+            // merupakan nama yang unik dan yang bisa terbilang langka
+            String ttl = "Padang,13 Oktober 2003";
+            /* lahir aja yang dipadang tinggal pindah- pindah
+                dari di jakarta
+                padang lagi
+                terakhir baru di lubuk basung
+            */
+            String wishlist = "Pergi ke China, Tinggal di New Zealand, Miliarder";
+            /*
+                alasan pergi ke china karena mau lanjut s2 di china
+                tinggal merupakan wishlist yang sangat diinginkan dikarenakan new zealand memiliki suasana yang tenang
+                miliarder biar bisa bahagiakan orang tua dan membeli sesuatu tanpa lihat harga
+            */
+            String motto = "Harus bisa semuanya biar orang tua bangga";
+            // semuanya harus bisa biar menjadi contoh bagi adik dan sepupu - sepupu
+            
+            System.out.println(Nama);
+            System.out.println(ttl);
+            System.out.println(wishlist);
+            System.out.println(motto);
+    }
+    
+} 
---
+++ Tugas3.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas3;
+
+/**
+ *
+ * @author Administrasi
+ */
+public class Tugas3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number =10;
+        char letter ='a';
+        boolean result =true;
+        String str ="Hello";
+        
+        System.out.println("Number = " +number);
+        System.out.println("Letter = " +letter);
+        System.out.println("Result = " +result);
+        System.out.println("Str = " +str);
+        // TODO code application logic here
+    }
+    
+}
