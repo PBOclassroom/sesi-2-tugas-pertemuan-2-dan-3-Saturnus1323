2b8f50d0cc95dd49bb373bb11c08dda9b8b10f89
Syaira Amani Fatihah <116801949+Syaira1323@users.noreply.github.com>
2023-09-19 14:55:48 +0700

    Add files via upload

---
+++ Project2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project2;
+
+/**
+ *
+ * @author Administrasi
+ */
+public class Project2 {
+
+    public static void main(String args[]) {
+        System.out.println("Single line comment above");
+        // Comment line 1
+        System.out.println("Multi line comments below");
+        /*Comment line 2
+          Comment line 3
+          Comment line 4
+        */
+    }
+}
